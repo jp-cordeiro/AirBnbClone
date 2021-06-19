a928df728403403ab84214e5c1164c1bbadf7900
JP <jpc.araujo@hotmail.com>
2021-06-19 17:21:52 -0300

    chore - render itens dynamically

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,30 +2,30 @@ import React from 'react';
 import { View, Image, Text } from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         style={styles.image}
         source={{
-          uri: 'https://content.r9cdn.net/rimg/himg/10/f3/2c/ostrovok-8312746-5c066de3e1015d49b154dd86eebb7d3ec70ac948-646249.jpg'
+          uri: post.image
         }}
       />
       {/* Bed & Bedrroms */}
-      <Text style={styles.bedrooms}>1 cama 1 quarto</Text>
+      <Text style={styles.bedrooms}>{post.title}</Text>
 
       {/* Type & Description */}
-      <Text style={styles.description}>Descrição</Text>
+      <Text style={styles.description}>{post.type}</Text>
 
       {/* Old price & New price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>R$ 150 </Text>
-        <Text style={styles.newPrice}> R$ 100 / noite </Text>
+        <Text style={styles.oldPrice}>R$ {post.oldPrice} </Text>
+        <Text style={styles.newPrice}> R$ {post.newPrice} / noite </Text>
       </Text>
 
       {/* Total price */}
-      <Text style={styles.totalPrice}>R$ 100</Text>
+      <Text style={styles.totalPrice}>R$ {post.totalPrice}</Text>
     </View>
   );
 };
